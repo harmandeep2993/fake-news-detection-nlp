4736eb42b926ab1ba4eed39bf3dff2cfd818cb7e
harmandeep2993 <harmandeep2993@gmail.com>
2026-02-25 22:45:33 +0100

    feat: implment vectorize functionality

---
+++ src/vectorize.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+def get_vectorize(
+        X_train,
+        X_test,
+        method: str = 'bow',
+        ngram_range: tuple=(1,1),
+        min_df:int = 1, 
+        max_df: float = 0.01,
+        max_features:int | None = 10000):
+    
+    if method == 'bow':
+
+        vectorizer = CountVectorizer(
+            ngram_range=ngram_range, 
+            min_df=min_df, 
+            max_df=max_df, 
+            max_features=max_features
+        )
+        print("I am Vectorizer using Bag of Words")
+    
+    elif method == 'tdidf':
+
+        vectorizer = TfidfVectorizer(
+            ngram_range=ngram_range, 
+            min_df=min_df, 
+            max_df=max_df, 
+            max_features=max_features
+        )
+        print("I am Vectorizer using TF-IDF")
+    
+    else:
+        raise ValueError("Vectorize method must be 'bow' or 'tfidf'")
+    
+    # Fit only on training data
+    X_train_vec = vectorizer.fit_transform(X_train)
+
+    # Transform test data
+    X_test_vec = vectorizer.transform(X_test)
+
+    print("Vectorization completed")
+    print("Train shape:", X_train_vec.shape)
+    print("Test shape :", X_test_vec.shape)
+
+    features = vectorizer.get_feature_names_out()
+
+    return vectorizer, features, X_train_vec, X_test_vec
