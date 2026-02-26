ec58a8af0ed39b1550d796cb98b42dd31328aa62
harmandeep2993 <harmandeep2993@gmail.com>
2026-02-26 02:13:55 +0100

    refact: add method, rounding and features_limit in metrics and reduce features to 5k

--- utils/vectorizeBOW.py
+++ utils/vectorizeBOW.py
@@ -14,7 +14,7 @@ def run_bow_experiment(
     ngram_range=(1,1),
     min_df:int | float = 10,
     max_df: float =1.0,
-    set_max_features=10000,
+    set_max_features=5000,
     comments=""
 ):
     
@@ -38,19 +38,17 @@ def run_bow_experiment(
     
     row = {
         "model": model.__class__.__name__,
+        "method": 'countVectorizer',
         "ngram_range": ngram_range,
         "min_df": min_df,
         "max_df": max_df,
-        "features_rec": len(vectorizer.get_feature_names_out()),
-        "max_features": set_max_features,
-        
-        
-        "test_accuracy":  accuracy_score(y_test, y_test_pred),
-        
-        "train_f1": f1_score(y_train, y_train_pred),
-        "test_f1":  f1_score(y_test, y_test_pred),
+        "max_feature": len(vectorizer.get_feature_names_out()),
+        "features_limit": set_max_features,
+        "train_acc": round(accuracy_score(y_train, y_train_pred), 4),
+        "test_acc":  round(accuracy_score(y_test, y_test_pred), 4),
+        "f1_train": round(f1_score(y_train, y_train_pred), 4),
+        "f1_test":  round(f1_score(y_test, y_test_pred),4),
         "gap_percent": round((train_f1 - test_f1) * 100, 2),
-        
         "comments": comments
     }
     
